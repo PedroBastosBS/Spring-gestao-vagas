e07138b66413ed9561aa74af7371eef1a1a1dfe9
PedroBastosB <pedrosenhor1@hotmail.com>
2024-02-03 09:25:10 -0400

    chore: create entity and controller

---
+++ src/main/java/br/com/pedrobastos/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,18 @@
+package br.com.pedrobastos.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.pedrobastos.gestao_vagas.modules.candidate.entities.CandidateEntity;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/candidate")
+
+public class CandidateController {
+    
+    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        
+    }
+}
